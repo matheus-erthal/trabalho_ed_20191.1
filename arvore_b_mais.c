d73c5ef2b4afa3115f59318d9ab690329b189cd6
Matheus Erthal Amâncio <matheusamancio@id.uff.br>
2019-05-31 08:53:55 -0300

    Adicionando busca(somente se a raiz for folha

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -3,14 +3,29 @@
 #endif
 
 #include <limits.h>
+#include <stdio.h>
 
 #include "arvore_b_mais.h"
 #include "lista_pizzas.h"
+#include "metadados.h"
+#include "no_folha.h"
+#include "pizza.h"
 
 
 int busca(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
 {
-	//TODO: Inserir aqui o codigo do algoritmo
+    TMetadados* meta = le_arq_metadados(nome_arquivo_metadados);
+    int pont_raiz = meta->pont_raiz;
+
+    // maior ou igual - direita
+    // menor - esquerda
+
+    if(meta->raiz_folha){
+        FILE* arq_folhas = fopen(nome_arquivo_dados, "rb");
+        fseek(arq_folhas, meta->pont_raiz, 0);
+        TNoFolha* raiz = le_no_folha(meta->d, arq_folhas);
+        return pont_raiz;
+    }
     return INT_MAX;
 }
 
