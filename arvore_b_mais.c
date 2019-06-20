4122367732afb51ab9a30dad7e89d26daec6e0d0
Matheus Erthal Amâncio <matheusamancio@id.uff.br>
2019-06-20 01:23:18 -0300

    Casos de inserção sem particionamento

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -47,10 +47,59 @@ int busca(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, char
     }
 }
 
-int insere(int cod, char *nome, char *descricao, float preco, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
-{
-	//TODO: Inserir aqui o codigo do algoritmo de insercao
-    return INT_MAX;
+// função que ajeita o vetor de clientes para inserção
+void _adapta_vetor(TPizza** clientes, int inicio, int m){
+    int i;
+    TPizza* escolhido = NULL;
+    TPizza* temp;
+    for(i = inicio; i <= m; i++){
+        temp = clientes[i];
+        clientes[i] = escolhido;
+        escolhido = temp;
+    }
+    return;
+}
+
+int insere(int cod, char *nome, char *categoria, float preco, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
+{   
+    // busca a posição onde o dado deve ser inserido
+	int posicao = busca(cod, nome_arquivo_metadados, nome_arquivo_indice, nome_arquivo_dados, d);
+    // abre o arquivo de dados
+    FILE* arq_dados = fopen(nome_arquivo_dados, "rb+");
+    // vai para a posição onde o dado deve ser inserido
+    fseek(arq_dados, posicao, SEEK_SET);
+    // lê a página da memória
+    TNoFolha* folha = le_no_folha(d, arq_dados);
+    // cria o novo registro
+    TPizza* nova_pizza = pizza(cod, nome, categoria, preco);
+    // se a página não estiver cheia
+    if(folha->m < 2 * d){
+        int i;
+        // descobre a posição que o novo registro deve ser inserido dentro da página
+        for(i = 0; i < folha->m && nova_pizza->cod >= folha->clientes[i]->cod; i++){
+            // se o registro já existe, retorna -1
+            if(folha->clientes[i]->cod == nova_pizza->cod) return -1;
+        }
+        // ajeita o vetor para receber o registro na posição correta
+        _adapta_vetor(folha->clientes, i, folha->m);
+        // atribui o novo registro a posição
+        folha->clientes[i] = nova_pizza;
+        // atualiza o valor de m da página
+        folha->m++;
+        // volta para a posição inicial da página no arquivo
+        fseek(arq_dados, posicao, SEEK_SET);
+        // salva a página no arquivo
+        salva_no_folha(d, folha, arq_dados);
+        // fecha o arquivo
+        fclose(arq_dados);
+        // retorna a posição da página
+        return posicao;
+    // se a página estiver cheia
+    }else{
+        printf("%d->", -1);
+        fclose(arq_dados);
+        return INT_MAX;
+    }
 }
 
 int exclui(int cod, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
