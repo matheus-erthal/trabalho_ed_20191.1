d3abc7810042d496fa0a2b4307b2a9d942e415c0
Matheus Erthal Amâncio <matheusamancio@id.uff.br>
2019-06-29 16:58:02 -0300

    Adicionando código de inserção com particionamento - não passa ainda nos testes

--- arvore_b_mais.c
+++ arvore_b_mais.c
@@ -4,6 +4,7 @@
 
 #include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "arvore_b_mais.h"
 #include "lista_pizzas.h"
@@ -60,53 +61,37 @@ void _adapta_vetor_sem_particao(TPizza** clientes, int inicio, int m){
     return;
 }
 
-// função que ajeita o vetor de clientes para inserção com partição
-void _adapta_vetor_com_particao(TPizza** p, TPizza** q, int d, TPizza* nova_pizza){
-    TPizza **temp = (TPizza**)malloc(sizeof(TPizza*) * (2 * d + 1));
+// função que particiona a folha passada e retorna a nova folha
+TNoFolha* _particiona_folha(TNoFolha* p, int d, TPizza* nova_pizza){
+    TNoFolha* q = no_folha(d, d+1, p->pont_pai, p->pont_prox);
+    TPizza **temp = (TPizza**) malloc(sizeof(TPizza*) * (2 * d + 1));
     int i;
     int posicionado = 0;
     for(i = 0; i < 2 * d + 1; i++){
         if(posicionado){
-            temp[i] = p[i-1];
+            temp[i] = p->clientes[i-1];
         }else{
-            if(nova_pizza->cod < p[i]->cod){
+            if(nova_pizza->cod < p->clientes[i]->cod){
                 temp[i] = nova_pizza;
                 posicionado = 1;
             }else{
-                temp[i] = p[i];
+                temp[i] = p->clientes[i];
             }
         }
     }
     for(i = 0; i < 2 * d + 1; i++){
         if(i < d){
-            p[i] = temp[i];
+            p->clientes[i] = temp[i];
         }else{
-            q[i-d] = temp[i];
+            q->clientes[i-d] = temp[i];
+            if(i < 2 * d){
+                p->clientes[i] = NULL;
+            }
         }
     }
-
     free(temp);
-}
-
-
-void particiona_no_folha(TNoFolha* folha, TPizza* nova_pizza, int d, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados){
-    TNoFolha* nova_folha = no_folha(d, d+1, folha->pont_pai, folha->pont_prox);
-    
-    _adapta_vetor_com_particao(folha->clientes, nova_folha->clientes, d, nova_pizza);
-
-    folha->m = d;
-
-    printf("m folha = %d/", folha->m);
-    
-
-    // FILE* arq_indice = fopen(nome_arquivo_indice, "rb");
-
-
-
-
-
-    
-
+    p->m = d; 
+    return q;
 }
 
 int insere(int cod, char *nome, char *categoria, float preco, char *nome_arquivo_metadados, char *nome_arquivo_indice, char *nome_arquivo_dados, int d)
@@ -121,9 +106,9 @@ int insere(int cod, char *nome, char *categoria, float preco, char *nome_arquivo
     TNoFolha* folha = le_no_folha(d, arq_dados);
     // cria o novo registro
     TPizza* nova_pizza = pizza(cod, nome, categoria, preco);
+    int i, j;
     // se a página não estiver cheia
     if(folha->m < 2 * d){
-        int i;
         // descobre a posição que o novo registro deve ser inserido dentro da página
         for(i = 0; i < folha->m && nova_pizza->cod >= folha->clientes[i]->cod; i++){
             // se o registro já existe, retorna -1
@@ -145,7 +130,54 @@ int insere(int cod, char *nome, char *categoria, float preco, char *nome_arquivo
         return posicao;
     // se a página estiver cheia
     }else{
-        particiona_no_folha(folha, nova_pizza, d, nome_arquivo_metadados, nome_arquivo_indice, nome_arquivo_dados);
+        TNoFolha* nova_folha = _particiona_folha(folha, d, nova_pizza);
+        fseek(arq_dados, 0L, SEEK_END);
+        int posicao_nova_folha = ftell(arq_dados);
+        nova_folha->pont_prox = folha->pont_prox;
+        folha->pont_prox = posicao_nova_folha;
+        fseek(arq_dados, posicao, SEEK_SET);
+        salva_no_folha(d, folha, arq_dados);
+        int escolhido = nova_folha->clientes[0]->cod;
+        FILE* arq_indice = fopen(nome_arquivo_indice, "rb+");
+        if(folha->pont_pai != -1){
+            fseek(arq_indice, folha->pont_pai, SEEK_SET);
+            TNoInterno* pai = le_no_interno(d, arq_indice);
+
+            if(pai->m < 2 * d){
+                for(i = 0; i < pai->m && pai->chaves[i] < escolhido; i++);
+                for(j = pai->m; j >= i; j--){
+                    pai->chaves[j] = pai->chaves[j - 1];
+                    pai->p[j+1] = pai->p[j];
+                }
+                pai->chaves[i] = escolhido;
+                pai->m++;
+                pai->p[i+1] = posicao_nova_folha;
+                nova_folha->pont_pai = folha->pont_pai;
+                printf("folha_antiga=%d", posicao);
+                printf("nova_folha=%d", posicao_nova_folha);
+                printf("no_interno=%d", folha->pont_pai);
+                fseek(arq_dados, posicao_nova_folha, SEEK_SET);
+                salva_no_folha(d, nova_folha, arq_dados);
+                fseek(arq_indice, folha->pont_pai, SEEK_SET);
+                salva_no_interno(d, pai, arq_indice);
+
+            }
+            // printf("codigo a inserir: %d\n", cod);
+            // printf("Folha 1:\n");
+            // imprime_no_folha(d, folha);
+            // printf("Folha 2:\n");
+            // imprime_no_folha(d, nova_folha);
+            // printf("Nó interno:\n");
+            // imprime_no_interno(d, pai);
+            fclose(arq_indice);
+            fclose(arq_dados);
+            return posicao;
+        }else{
+            printf("sem pai->");
+        }
+        // imprime_no_folha(d, folha);
+        // imprime_no_folha(d, nova_folha);
+
         fclose(arq_dados);
         return INT_MAX;
     }
